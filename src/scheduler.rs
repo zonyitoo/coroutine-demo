d139c4a5dbbca917384e88666805087d68d7ed9a
Y. T. Chung <zonyitoo@gmail.com>
2015-05-23 23:57:03 +0800

    add licenses

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,4 +1,23 @@
+// The MIT License (MIT)
 
+// Copyright (c) 2015 Rustcc Developers
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 use std::thread;
 use std::sync::mpsc::{channel, Sender, Receiver, TryRecvError};
@@ -15,7 +34,7 @@ use std::sync::Arc;
 use std::collections::VecDeque;
 
 use coroutine::spawn;
-use coroutine::coroutine::{State, Handle, Coroutine};
+use coroutine::coroutine::{State, Handle, Coroutine, Options};
 
 use deque::{BufferPool, Stealer, Worker, Stolen};
 
@@ -106,11 +125,16 @@ impl Scheduler {
 
     pub fn spawn<F>(f: F)
             where F: FnOnce() + Send + 'static {
+        let coro = Coroutine::spawn(f);
+        Scheduler::current().ready(coro);
 
-        let coro = spawn(f);
+        Coroutine::sched();
+    }
 
-        let sc = Scheduler::current();
-        sc.ready(coro);
+    pub fn spawn_opts<F>(f: F, opt: Options)
+            where F: FnOnce() + Send + 'static {
+        let coro = Coroutine::spawn_opts(f, opt);
+        Scheduler::current().ready(coro);
 
         Coroutine::sched();
     }
