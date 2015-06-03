a22af42beaa88f835454956a31a3ab987eaca91a
Y. T. Chung <zonyitoo@gmail.com>
2015-06-03 21:16:50 +0800

    removed unused feature

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#![feature(libc, std_misc)]
+#![feature(libc)]
 
 extern crate coroutine;
 extern crate num_cpus;
