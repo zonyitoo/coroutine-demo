adb6cdf1d7480ab8c6b2d2d851dd8a6c551c5afc
Y. T. Chung <zonyitoo@gmail.com>
2015-06-06 23:17:49 +0800

    fixed tcp echo

--- src/bin/echoserver.rs
+++ src/bin/echoserver.rs
@@ -10,7 +10,7 @@ use std::net::SocketAddr;
 use clap::{Arg, App};
 
 use cosupport::scheduler::Scheduler;
-use cosupport::net::tcp::TcpSocket;
+use cosupport::net::tcp::TcpListener;
 
 fn main() {
     env_logger::init().unwrap();
@@ -27,13 +27,8 @@ fn main() {
     let bind_addr = matches.value_of("BIND").unwrap().to_owned();
 
     Scheduler::spawn(move|| {
-        let addr = bind_addr.parse().unwrap();
-        let server = match &addr {
-            &SocketAddr::V4(..) => TcpSocket::v4(),
-            &SocketAddr::V6(..) => TcpSocket::v6(),
-        }.unwrap();
-        server.set_reuseaddr(true).unwrap();
-        let server = server.listen(64).unwrap();
+        let addr: SocketAddr = bind_addr.parse().unwrap();
+        let server = TcpListener::bind(&addr).unwrap();
 
         info!("Listening on {:?}", server.local_addr().unwrap());
 
